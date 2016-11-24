1413420207d2f210b0a7839ba51178e0781d437f
Erik Dubbelboer <erik@dubbelboer.com>
2016-11-24 03:32:27 +0000

    Fix package path (fix #8) and go fmt

--- gspt_test.go
+++ gspt_test.go
@@ -1,47 +1,43 @@
-
 package gspt
 
 import (
-  "testing"
-  "bytes"
-  "strings"
-  "time"
+	"bytes"
+	"strings"
+	"testing"
+	"time"
 
-  "encoding/binary"
-  "encoding/hex"
+	"encoding/binary"
+	"encoding/hex"
 
-  "crypto/md5"
-  "os/exec"
+	"crypto/md5"
+	"os/exec"
 )
 
-
 func randomMD5() string {
-  str    := md5.New()
-  random := new(bytes.Buffer)
+	str := md5.New()
+	random := new(bytes.Buffer)
 
-  binary.Write(random, binary.LittleEndian, time.Now().UTC().UnixNano())
+	binary.Write(random, binary.LittleEndian, time.Now().UTC().UnixNano())
 
-  str.Write(random.Bytes())
+	str.Write(random.Bytes())
 
-  return hex.EncodeToString(str.Sum(nil))
+	return hex.EncodeToString(str.Sum(nil))
 }
-
 
 func TestSetProcTitle(t *testing.T) {
-  if HaveSetProcTitle == HaveNone {
-    t.SkipNow()
-  }
+	if HaveSetProcTitle == HaveNone {
+		t.SkipNow()
+	}
 
-  title := randomMD5()
+	title := randomMD5()
 
-  SetProcTitle(title)
+	SetProcTitle(title)
 
-  out, err := exec.Command("/bin/ps", "ax").Output()
-  if err != nil {
-    // No ps available on this platform.
-    t.SkipNow()
-  } else if !strings.Contains(string(out), title) {
-    t.FailNow()
-  }
+	out, err := exec.Command("/bin/ps", "ax").Output()
+	if err != nil {
+		// No ps available on this platform.
+		t.SkipNow()
+	} else if !strings.Contains(string(out), title) {
+		t.FailNow()
+	}
 }
-
